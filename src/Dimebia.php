a8c0691f600347c40ef7a66e88dfc486b824734e
Alex <alex@laialex.com>
2025-03-25 10:12:47 +0800

    initial

---
+++ src/Dimebia.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @author Alex Lai
+ * @email alex@laialex.com
+ * @website https://laialex.com
+ * @Date 2025/3/25 9:45
+ */
+class Dimebia
+{
+
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alaikis/dimabia',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8b9863c6625391579771da7577e85e37635abc4b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'alaikis/dimabia' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8b9863c6625391579771da7577e85e37635abc4b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
