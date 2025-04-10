c0ab8a6435da05cb58410f496a1f0d94f0c13176
Alex <alex@laialex.com>
2025-04-10 15:48:28 +0800

    the v1 release

--- src/Dimebia.php
+++ src/Dimebia.php
@@ -1,5 +1,5 @@
 <?php
-
+namespace Alaikis\Dimebia;
 /**
  * @author Alex Lai
  * @email alex@laialex.com
@@ -9,7 +9,7 @@
 class Dimebia
 {
 
-    protected mixed $baseUrl="https://api.dimebia.alaikis.com";
+    protected mixed $baseUrl="https://api.hottol.com/dimebia";
     protected mixed $token;
     protected mixed $version="v1";
 
@@ -58,19 +58,79 @@ class Dimebia
      * @Author Alex
      * @Date 2025/3/25 10:33
      */
-    public function createPayment($requestParams) {
-        return $this->httpFetch("/payment/create", $requestParams);
+    public function paymentApply($requestParams) {
+        return $this->httpFetch("/payments/apply", $requestParams);
     }
 
-    public function balances() {}
-
-    public function shipments()
-    {
+    /**
+     * get the payment info by bill id
+     * @param $billId
+     * @return string
+     * @Author Alex
+     * @Date 2025/4/10 15:43
+     */
+    public function getPaymentById($billId) {
+        return $this->httpFetch("/payments/get", ["transactionId"=>$billId]);
     }
 
-    public function orderRefunds(){
+    /**
+     * get the payment list
+     * @param $filter
+     * @return string
+     * @Author Alex
+     * @Date 2025/4/10 15:43
+     */
+    public function getPayments($filter) {
+        return $this->httpFetch("/payments/list", $filter);
     }
 
+    /**
+     * cancel the payment
+     * @param $billId
+     * @return string
+     * @Author Alex
+     * @Date 2025/4/10 15:43
+     */
+    public function paymentCancel($billId) {
+        return $this->httpFetch("/payments/cancel", ["transactionId"=>$billId]);
+    }
+
+    /**
+     * refund the payment
+     * @param $billId
+     * @param $refundAmount
+     * @return string
+     * @Author Alex
+     * @Date 2025/4/10 15:43
+     */
+    public function paymentRefund($billId,$refundAmount) {
+        return $this->httpFetch("/payments/refund", [
+            "transactionId"=>$billId,
+            "refundAmount"=>$refundAmount,
+        ]);
+    }
+
+    /**
+     * get the balances will group by currency
+     * @param $filter
+     * @return string
+     * @Author Alex
+     * @Date 2025/4/10 15:43
+     */
+    public function balances($filter) {
+        return $this->httpFetch("/balances/list", $filter);
+    }
+
+    /**
+     * mark the order as shipped
+     * @param $filter
+     * @return string
+     * @Author Alex
+     * @Date 2025/4/10 15:44
+     */
+    public function shipments($filter) {
+        return $this->httpFetch("/shipments/list", $filter);
+    }
 
 
 }
