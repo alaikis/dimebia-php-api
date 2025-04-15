e8b413a0e77a094f8fd24f9e84d1329940eddd4e
Alex <alex@laialex.com>
2025-04-15 16:29:56 +0800

    flexible

--- src/Traits/HttpTrait.php
+++ src/Traits/HttpTrait.php
@@ -14,7 +14,7 @@ trait HttpTrait
     private string $baseUrl = "https://api.hottol.com/dimebia/user/";
     private string $version="v1";
 
-    private string  $CONTENT_TYPE;
+    private string  $CONTENT_TYPE="";
     /**
      * make http request
      * @param $uri
